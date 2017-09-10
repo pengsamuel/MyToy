06c74b35cf8a921222bd0c8464db0e4360bd9ac9
pengsamuel <samuelpeng@vip.qq.com>
2017-09-10 21:04:17 +0800

    Add files via upload
    for Game Dont Starv on TGP
    load and save game record

---
+++ DonStarv_load.py
@@ -0,0 +1,28 @@
+﻿#!/usr/bin/python
+
+# -*- coding:utf-8 -*-  
+
+import shutil
+import os
+
+path = "D:\\WeGame\\rail_apps\\饥荒：单机版\\rail_user_data\\2000013\\76561197980970883\\cloud_storage\\user_space\\"
+upath = unicode(path, 'utf-8')
+
+sortlist = os.listdir(upath)
+sortlist.sort()
+
+print '##################################################################'
+print '##################   LOAD Don Starv   ############################'
+print '##################################################################'
+
+print "will rm dir:"+sortlist[0]
+shutil.rmtree(upath+sortlist[0]) 
+
+print "copy data from:"+sortlist[-1]
+shutil.copytree(upath+sortlist[-1], upath+sortlist[0], False)
+
+
+print '##################################################################'
+print '##################   LOAD Don Starv End   ########################'
+print '##################################################################'
+
---
+++ DonStarv_save.py
@@ -0,0 +1,29 @@
+﻿#!/usr/bin/python
+
+# -*- coding:utf-8 -*-  
+
+import shutil
+import time
+
+#此脚本文件需以UTF-8格式编码
+src = "D:\\WeGame\\rail_apps\\饥荒：单机版\\rail_user_data\\2000013\\76561197980970883\\cloud_storage\\user_space\\2199037600743130074"
+
+dst = "D:\\WeGame\\rail_apps\\饥荒：单机版\\rail_user_data\\2000013\\76561197980970883\\cloud_storage\\user_space\\2199037600743130074_save_"
+
+print '##################################################################'
+print '##################   SAVE Don Starv   ############################'
+print '##################################################################'
+#加上时间值作为后缀
+strtime = time.strftime('%Y-%m-%d_%H-%M-%S',time.localtime(time.time()))
+
+#必须转换成内部编码格式
+usrc = unicode(src, 'utf-8')
+udst = unicode(dst+strtime, 'utf-8')
+
+#拷贝目录
+shutil.copytree(usrc, udst, False)
+
+
+print '##################################################################'
+print '##################   SAVE Don Starv End   ########################'
+print '##################################################################'
